c8eb4569751c195f809af87b8c4d7e3eeebe06f7
taha.amine <taha.amine@CD003101>
2021-10-13 12:31:19 +0200

    add input

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,7 +1,15 @@
-import Screen from '../Screen';
+import Screen from "../Screen";
+import "./style.css";
 
 export default class SearchScreen extends Screen {
   constructor() {
-    return super({ title: 'Search' });
+    const content = document.createElement("div");
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.classList.add("search");
+
+    content.appendChild(input);
+
+    return super({ title: "Search", children: content });
   }
 }
