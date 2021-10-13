4e1f988b2393c532f499607bab1367d4465bc00a
taha.amine <taha.amine@cdbdx.biz>
2021-10-13 16:17:02 +0200

    add fetch

--- src/Search/index.js
+++ src/Search/index.js
@@ -2,10 +2,25 @@ import Screen from "../Screen";
 import Input from "../Input";
 import "./style.css";
 
-export default class SearchScreen extends Screen {
-  constructor() {
-    const input = new Input({ title: "search" });
+export default class SearchScreen {
+  constructor() {}
+  render() {
+    const content = document.createElement("div");
 
-    return super({ title: "Search", children: input.render() });
+    const input = new Input({
+      title: "search",
+      onChange: (value) => this.fetchOmdb(value),
+    });
+
+    content.appendChild(input.render());
+
+    const screen = new Screen({ title: "Search", children: content });
+    return screen.render();
+  }
+
+  fetchOmdb(value) {
+    var apiKey = "b63da2a6";
+
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${value}`);
   }
 }
